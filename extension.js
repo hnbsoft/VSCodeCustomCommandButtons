a37d3810a99b9c4c8a8808fc82f1e937b299b3c2
hnb-mac <hnbsoft@outlook.com>
2023-02-23 20:27:31 +0800

    Integrated hnb-shell-checker terminal command

--- extension.js
+++ extension.js
@@ -2,6 +2,47 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
+// Helper - Returns the current opened(actived) file path or `undefined`
+function getCurrentFilePath() {
+    // The return value
+    var retPath = undefined;
+
+    // Find the current editor
+    let currentEditor = vscode.window.activeTextEditor;
+    if (currentEditor) {
+        if (!(currentEditor.document.isUntitled)) {
+            retPath = currentEditor.document.fileName;
+        }
+    }
+
+    return retPath;
+}
+
+// Helper - Execute a command in Terminal
+function executeCommandInTerminal(commandText) {
+    let currentTerminal = vscode.window.activeTerminal;
+    if (currentTerminal) {
+        // There is an active Terminal here, execute the command in this Terminal
+        currentTerminal.sendText(commandText);
+    }
+    else {
+        // No active Terminal found, just show a message to user.
+        vscode.window.showInformationMessage('HNB: Command not executed in terminal (No active terminal).');
+    }
+}
+
+// Run a given `Terminal Command` with `Current File Path` as its argument.
+function runTerminalCommandWithCurrentFilePath(commandName) {
+    let currentFilePath = getCurrentFilePath();
+    if (currentFilePath) {
+        let commandWithArgument = commandName + ' ' + '"' + currentFilePath + '"';
+        executeCommandInTerminal(commandWithArgument);
+    }
+    else {
+        vscode.window.showInformationMessage('HNB: Failed to get current file (No active editor, untitled, etc).');
+    }
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -17,14 +58,14 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('customcommandbuttons.command.hnbShellChecker', function () {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CustomCommandButtons!');
-	});
-
-	context.subscriptions.push(disposable);
+    // The shell checker terminal command
+    context.subscriptions.push(
+        vscode.commands.registerCommand('customcommandbuttons.command.hnbShellChecker', function () {
+            // The code you place here will be executed every time your command is executed
+            runTerminalCommandWithCurrentFilePath('hnb-shell-checker');
+        })
+    );
 }
 
 // This method is called when your extension is deactivated
